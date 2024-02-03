0b53633058ea61265401e3a15b7155af81ffbb47
arjunpnair7 <76981445+arjunpnair7@users.noreply.github.com>
2024-02-02 22:01:23 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
   # Optional: You can pass data to the template here for dynamic content
   data = {"message": "Hello from Python!"}
-  return render_template("test.html", data=data)
+  return render_template("index.html", data=data)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=False
